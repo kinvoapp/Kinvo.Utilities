3bf3fda661731afea0a94b00cb4d6bda16811c3b
iFalcao <vbsfalcao@gmail.com>
2020-01-26 20:54:10 -0300

    Create GetUntilOrEmpty and StringBetween extension methods for strings

--- Kinvo.Utilities/Extensions/StringExtensions.cs
+++ Kinvo.Utilities/Extensions/StringExtensions.cs
@@ -13,5 +13,84 @@ namespace Kinvo.Utilities.Extensions
             var provider = CultureInfo.InvariantCulture;
             return DateTime.ParseExact(inputDate, format, provider);
         }
+
+        public static string GetUntilOrEmpty(this string text, string stopAt = "-")
+        {
+            if (!String.IsNullOrWhiteSpace(text))
+            {
+                int charLocation = text.IndexOf(stopAt, StringComparison.Ordinal);
+
+                if (charLocation > 0)
+                {
+                    return text.Substring(0, charLocation);
+                }
+            }
+
+            return String.Empty;
+        }
+
+        public static string StringBetween(this string text, string begin, string end)
+        {
+            return text.StringBetween(begin, end, 1);
+        }
+
+        public static string StringBetween(this string text, string begin, string end, int numorder)
+        {
+            return text.StringBetween(begin, end, numorder, false);
+        }
+
+        public static string StringBetween(this string text, string begin, string end, int numorder, bool revert)
+        {
+            if ((text != null) && (text != string.Empty))
+            {
+                char[] strArrayRevert;
+                if (revert)
+                {
+                    strArrayRevert = text.ToCharArray();
+                    Array.Reverse(strArrayRevert);
+                    text = new string(strArrayRevert);
+                }
+                int i = 0;
+                int startingPosition = -1;
+                int finalPosition = -1;
+                int startIndex = 0;
+                while ((i < numorder) && (startIndex < text.Length))
+                {
+                    startingPosition = text.IndexOf(begin, startIndex, StringComparison.OrdinalIgnoreCase);
+                    finalPosition = ((startingPosition + begin.Length) < text.Length)
+                        ? text.IndexOf(end, startingPosition + begin.Length, StringComparison.OrdinalIgnoreCase)
+                        : -1;
+
+                    if ((startingPosition != -1) && (finalPosition != -1))
+                    {
+                        i++;
+                        startIndex = finalPosition + 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (((startingPosition != -1) && (finalPosition != -1)) && (i == numorder))
+                {
+                    try
+                    {
+                        string result = text.Substring(startingPosition + begin.Length, finalPosition - (startingPosition + begin.Length));
+                        if (revert)
+                        {
+                            strArrayRevert = result.ToCharArray();
+                            Array.Reverse(strArrayRevert);
+                            result = new string(strArrayRevert);
+                        }
+                        return result;
+                    }
+                    catch
+                    {
+                        return null;
+                    }
+                }
+            }
+            return null;
+        }
     }
 }

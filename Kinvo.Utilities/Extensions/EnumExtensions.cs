9d4e3fb723253649296e3cd5a9ff59b3c3e350de
Ícaro Falcão <vbsfalcao@gmail.com>
2020-03-19 10:25:25 -0300

    Add EnumExtensions

---
+++ Kinvo.Utilities/Extensions/EnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Kinvo.Utilities.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription<T>(this T source)
+        {
+            FieldInfo fi = source.GetType().GetField(source.ToString());
+
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(
+                typeof(DescriptionAttribute), false);
+
+            return (attributes != null && attributes.Length > 0) ? attributes[0].Description : source.ToString();
+        }
+    }
+}

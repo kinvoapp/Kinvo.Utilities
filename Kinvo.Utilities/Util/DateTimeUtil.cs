a0324c1b7a42ba699bd0ce2c4070ea1bf1fdebf6
Ícaro Falcão <vbsfalcao@gmail.com>
2020-04-22 22:30:56 -0300

    Add metthods for DateTimeUtil

--- Kinvo.Utilities/Util/DateTimeUtil.cs
+++ Kinvo.Utilities/Util/DateTimeUtil.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Kinvo.Utilities.Util
 {
@@ -11,6 +8,44 @@ namespace Kinvo.Utilities.Util
         public static int GetAmountOfMonthsBetweenDates(DateTime date1, DateTime date2)
         {
             return Convert.ToInt32(date1.Subtract(date2).Days / (365.25 / 12));
+        }
+
+        public static DateTime GetSouthAmericaDateTimeNow()
+        {
+            DateTime timeUtc = DateTime.UtcNow;
+            TimeZoneInfo kstZone = TimeZoneInfo.FindSystemTimeZoneById("E. South America Standard Time");
+            return TimeZoneInfo.ConvertTimeFromUtc(timeUtc, kstZone);
+        }
+
+        public static DateTime? GetMonthlyDateFromString(string inputDate)
+        {
+            if (string.IsNullOrEmpty(inputDate))
+                return null;
+
+            var provider = CultureInfo.InvariantCulture;
+            var format = "dd/MM/yyyy";
+
+            return DateTime.ParseExact("01/" + inputDate, format, provider);
+        }
+
+        public static DateTime ParseWithFallbacks(string inputDate, params string[] formats)
+        {
+            if (formats.Length == 0)
+                throw new ArgumentException();
+
+            for (int index = 0; index < formats.Length; index++)
+            {
+                var currentFormat = formats[index];
+                var successfullyParsed = DateTime.TryParseExact(inputDate,
+                                                                currentFormat,
+                                                                CultureInfo.InvariantCulture,
+                                                                DateTimeStyles.None,
+                                                                out var parsedDate);
+                if (successfullyParsed)
+                    return parsedDate;
+            }
+
+            throw new Exception("Couldn't convert to DateTime with the available formats");
         }
     }
 }

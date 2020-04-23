f9127ca9db8abcaff2fd3691b09849b240a6eb35
Ícaro Falcão <vbsfalcao@gmail.com>
2020-04-22 22:30:29 -0300

    Create ConvertUtil

---
+++ Kinvo.Utilities/Util/ConvertUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Kinvo.Utilities.Util
+{
+    public static class ConvertUtil
+    {
+        public static DateTime? TryParseNullableDateTime(string dateTimeString)
+        {
+            return DateTime.TryParse(dateTimeString, CultureInfo.GetCultureInfo("pt-BR"), DateTimeStyles.None, out var parsedValue)
+                ? (DateTime?)parsedValue
+                : null;
+        }
+
+        public static decimal? TryParseNullableDecimal(string decimalString)
+        {
+            return decimal.TryParse(decimalString, NumberStyles.Float, CultureInfo.GetCultureInfo("pt-BR"), out var parsedValue)
+                ? (decimal?)parsedValue
+                : null;
+        }
+
+        public static long? TryParseNullableLong(string longString)
+        {
+            return long.TryParse(longString, out var parsedValue)
+                ? (long?)parsedValue
+                : null;
+        }
+    }
+}

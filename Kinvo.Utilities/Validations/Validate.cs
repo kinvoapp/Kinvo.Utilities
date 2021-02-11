346dc96132b060fbc1a0b5c6a3a5485457a4da67
Titowisk <rabelo51@gmail.com>
2021-02-11 14:32:09 -0300

    Correção manual do revert feito pelo Github

--- Kinvo.Utilities/Validations/Validate.cs
+++ Kinvo.Utilities/Validations/Validate.cs
@@ -194,9 +194,7 @@ namespace Kinvo.Utilities.Validations
         /// <param name="email">mail adress</param>
         public static bool IsValidEmail(string emailAddress)
         {
-            string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
-                 @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
-                 @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+            string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,8}|[0-9]{1,8})(\]?)$";
 
             Regex rg = new Regex(strRegex);
 

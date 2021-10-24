88a2d1bbef36b694b3b1cb56c76f764048672c19
tarcisiobruni <tarcisiobruni@hotmail.com>
2021-10-24 17:40:00 -0300

    test: adiciona casos de teste para validação de cpf e cnpj

---
+++ Kinvo.Utilities.Test/Validations/CNPJValidationTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Kinvo.Utilities.Test.Validations
+{
+    public class CNPJValidationTest
+    {
+        [Theory]
+        [InlineData("27.883.844/0001-84", true)]
+        [InlineData("29.750.279/0001-02", true)]
+        [InlineData("88.408.631/0001-95", true)]
+        [InlineData("01.234.567/8910-11", false)]
+        [InlineData("1.234.567/8910-11", false)]
+        [InlineData("234.567/8910-11", false)]
+        public void ShouldValidateCorrectly(string document, bool result)
+        {
+            var isValid = Utilities.Validations.Validate.IsValidCNPJ(document);
+            result.Should().Be(isValid);
+        }
+    }
+}
---
+++ Kinvo.Utilities.Test/Validations/CPFValidationTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Kinvo.Utilities.Test.Validations
+{
+    public class CPFValidationTest
+    {
+        [Theory]
+        [InlineData("123.456.789-12", false)]
+        [InlineData("529.982.247-25", true)]
+        [InlineData("777.777.777-77", false)]
+        [InlineData("000.000.000-00", false)]
+        [InlineData("151.476.070-30", true)]
+        [InlineData("151.476.07030", true)]
+        [InlineData("151476070-30", true)]
+        public void ShouldValidateCorrectly(string document, bool result)
+        {
+            var isValid = Utilities.Validations.Validate.IsValidCPF(document);
+            result.Should().Be(isValid);
+        }
+    }
+}
